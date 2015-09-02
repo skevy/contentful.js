2c805abd651ce6640ee254fe376b767fd60f46b5
Adam Miskiewicz <adam.skevy@mac.com>
2015-09-02 13:07:31 -0400

    turning off xsrf

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ var Client = redefine.Class({
     var params = {
       headers: {},
       method: 'get',
+      xsrfEnabled: false,
       url: [
         this.options.secure ? 'https' : 'http',
         '://',
